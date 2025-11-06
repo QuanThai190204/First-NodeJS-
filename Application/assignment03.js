cb4ce6f785998d71ab081375a14c67c81d7a94b5
QuanThai190204 <quan190204@gmail.com>
2025-11-06 13:17:50 -0500

    Successfully access Postgresql through NodeJS

---
+++ Application/assignment03.js
@@ -0,0 +1,51 @@
+const { Pool } = require('pg');
+
+// Create a new pool instance
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    port: 5432,
+    password: 'postgres',
+    database: 'assignment03'
+});
+
+// Function to get all students
+async function getAllStudents() {
+    const res = await pool.query('SELECT * FROM students');
+    return res.rows;
+}
+
+
+// Function to add a new student
+async function addStudent(first_name, last_name, email, enrollment_date) {
+    const res = await pool.query('INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES ($1, $2, $3, $4) RETURNING *',
+        [first_name, last_name, email, enrollment_date]);
+}
+
+// Function to update a student's email
+async function updateStudentEmail(student_id, new_email) {
+    const res = await pool.query('UPDATE students SET email = $1 WHERE student_id = $2 RETURNING *',
+        [new_email, student_id]);
+}
+
+// Function to delete a student
+async function deleteStudent(student_id) {
+    const res = await pool.query('DELETE FROM students WHERE student_id = $1 RETURNING *',  
+        [student_id]); 
+}
+// Cleanup
+async function closePool() {
+    await pool.end();
+    console.log('Pool closed');
+}
+
+// Example usage
+(async () => {
+    //await addStudent('Ben', 'Holmes', 'ben.holmes@gmail.com', '2020-010-12')
+    //await updateStudentEmail(5, 'BEN.HOLMES@gmail.com');
+    //await deleteStudent(5);
+    const students = await getAllStudents();
+    console.log(students);
+    await closePool();
+})();
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# First-NodeJS-
+HOANG QUAN THAI - 101296950
+
+INSTALL INSTRUCTIONS: npm i pg 
+
+LAUNCH INSTRUCTIONS:  node assignment03.js
+                                    
+
+VIDEO DEMONSTRATION: https://youtu.be/bEq_RNk2o54
